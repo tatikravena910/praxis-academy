620bd8743cd4798996a702da76fa51f872752c87
tatikravena910 <tatikravena985@gail.com>
2022-08-25 09:46:25 +0700

    First Commit

---
+++ kemampuan-dasar/novice/02-04/5. Data Structures/index.py
@@ -0,0 +1,12 @@
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+print(fruits.count('apple'))
+print(fruits.count('tangerine'))
+print(fruits.index('banana'))
+fruits.index('banana', 4)
+fruits.reverse()
+print(fruits)
+fruits.append('grape')
+print(fruits)
+fruits.sort()
+print(fruits)
+print(fruits.pop())
