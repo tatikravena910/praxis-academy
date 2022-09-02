75131f2b95042293fc4f8b6e79b9fa82e699d991
tatikravena910 <tatikravena985@gail.com>
2022-09-02 11:43:08 +0700

    First Comit

---
+++ kemampuan-dasar/novice/03-05/app/main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+import psycopg2
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="contoh", 
+        user="postgres",
+        password="postgres12345"
+    )
+    curs = conn.cursor()
+    if request.method == "POST":
+        nama = (request.form.get("nama"))
+        detail = (request.form.get("detail"))
+        query = f"insert into buah(nama, detail) values('{nama}', '{detail}')" 
+        curs.execute(query)
+        conn.commit()
+        curs.close()
+        conn.close()
+        #    print(20*"=")
+        #    print(nama)
+        #    print(detail)
+        #    print(20*"=")
+
+    print(request.method)
+    query = f"select * from buah"
+    curs.execute(query)
+    data = curs.fetchall()
+    # data = ["apel", "pear", "anggur"]
+    return render_template("index.html", context=data)
+
+if __name__ == "__main__":
+    app.run()
