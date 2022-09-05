c72be3515ef49da17e84b1cd8d751f638d3c726c
tatikravena910 <tatikravena985@gail.com>
2022-09-05 12:01:29 +0700

    First Commit

--- kemampuan-dasar/novice/03-05/app/main.py
+++ kemampuan-dasar/novice/03-05/app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import psycopg2
 
 app = Flask(__name__)
@@ -31,6 +31,39 @@ def index():
     data = curs.fetchall()
     # data = ["apel", "pear", "anggur"]
     return render_template("index.html", context=data)
+
+@app.route("/detail/<buah_id>")
+def detail(buah_id):
+    conn = psycopg2.connect(
+        host="localhost",
+        database="contoh",
+        user="postgres",
+        password="postgres12345"
+    )
+    curs = conn.cursor()
+    query = f"select * from buah where id = {buah_id}"
+    curs.execute(query)
+    data = curs.fetchone()
+    curs.close()
+    conn.close()
+    print(data)
+    return render_template("detail.html", context=data)
+
+@app.route("/delete/<buah_id>")
+def delete(buah_id):
+    conn = psycopg2.connect(
+        host="localhost",
+        database="contoh",
+        user="postgres",
+        password="postgres12345"
+    )
+    curs = conn.cursor()
+    query = f"delete from buah where id = {buah_id}"
+    curs.execute(query)
+    conn.commit()
+    curs.close()
+    conn.close()
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run()
